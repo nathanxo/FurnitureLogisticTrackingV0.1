7a0b34ea42174e565aaf7f9d28f3bbe2adb17ee6
nathanxo <48918877+nathanxo@users.noreply.github.com>
2023-08-07 15:26:58 +0100

    final V1

--- getTrackingURL.py
+++ getTrackingURL.py
@@ -4,34 +4,34 @@ import string
 import sys
 import json
 
-def randomString(stringLength=10):
-	return ''.join(random.choice(string.ascii_lowercase) for i in range(stringLength))
+#def randomString(stringLength=10):
+#	return ''.join(random.choice(string.ascii_lowercase) for i in range(stringLength))
 
 #Passed in from the AJAX call.
-customerOrderNo = 'TOBS22272' #trackingNoInput
+#customerOrderNo = 'TOBS22272' #trackingNoInput
 
-url = 'https://www.go2stream.net/API/oauth'
-payload = "grant_type=client_credentials&client_id=<"CHANGE">&client_secret=<"CHANGE">"
-headers = {
-    'content-type': 'application/x-www-form-urlencoded',
-    'cache-control': "no-cache",
-    'Stream-Nonce': randomString(30)
-}
-response = requests.post(url, data=payload, headers=headers)
+#url = 'https://www.go2stream.net/API/oauth'
+#payload = "grant_type=client_credentials&client_id=<"CHANGE">&client_secret=<"CHANGE">"
+#headers = {
+#    'content-type': 'application/x-www-form-urlencoded',
+#    'cache-control': "no-cache",
+#    'Stream-Nonce': randomString(30)
+#}
+#response = requests.post(url, data=payload, headers=headers)
 
-if response.status_code != 200:
+#if response.status_code != 200:
 	# Order Not Found, Show warning to user
-	print("NO RESPONSE", "Got Status Code: ", response.status_code, "GotResp:", response)
-	sys.exit()
+#	print("NO RESPONSE", "Got Status Code: ", response.status_code, "GotResp:", response)
+#	sys.exit()
     
 # Read the response
-json_response = response.json()
-access_token = json_response.get("access_token", "")
+#json_response = response.json()
+#access_token = json_response.get("access_token", "")
 
-if access_token == "":
+#if access_token == "":
 	# Order Not Found, Show warning to user
-	print("NO ACCESS TOKEN")
-	sys.exit()
+#	print("NO ACCESS TOKEN")
+#	sys.exit()
 
 #access_token = '121212'
 
@@ -39,23 +39,23 @@ if access_token == "":
 # 	#Order Not Found, Show warning to user
 # 	print("Order not found.")
 
-url = 'https://www.go2stream.net/API/orders/status/'+customerOrderNo
-headers = {'Accept': "application/json", 'Stream-Nonce': randomString(30), 'Stream-Party': "EXPRE000000000002", 'Authorization': "Bearer "+access_token, 'cache-control': "no-cache",}
+#url = 'https://www.go2stream.net/API/orders/status/'+customerOrderNo
+#headers = {'Accept': "application/json", 'Stream-Nonce': randomString(30), 'Stream-Party': "EXPRE000000000002", 'Authorization': "Bearer "+access_token, 'cache-control': "no-cache",}
 
-responseOrder = requests.get(url, headers=headers)
+#responseOrder = requests.get(url, headers=headers)
 
-if responseOrder.status_code != 200 and responseOrder.status_code != 422:
+#if responseOrder.status_code != 200 and responseOrder.status_code != 422:
 	#Order Not Found, Show warning to user
-	print("Order not found.", "Response: ", responseOrder)
+#	print("Order not found.", "Response: ", responseOrder)
 
 #Read the response
-json_response_order = responseOrder.json()
-response_body = json_response_order.get("response", "")
-tracking_url = response_body.get("trackingURL", "")
+#json_response_order = responseOrder.json()
+#response_body = json_response_order.get("response", "")
+#tracking_url = response_body.get("trackingURL", "")
 
-if tracking_url == "":
+#if tracking_url == "":
 	#Order Not Found, Show warning to user
-	print("Order not found.")
+#	print("Order not found.")
 
 #Pass this back to the AJAX then you can redirect the user via Javascript
-print(tracking_url)
+#print(tracking_url)
